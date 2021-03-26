37d9b3625d52b298eb82aebe2e7bb0396e655a0b
VicenteFernandezCampos <80338998+VicenteFernandezCampos@users.noreply.github.com>
2021-03-26 17:23:43 -0300

    carpetas

---
+++ Assets/Scripts/Cangrejo_Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cangrejo_Movement : MonoBehaviour
+{
+    public Transform transform;
+    public bool izquierda;
+    public bool derecha;
+    //izquierda es True y derecha será False
+    public Collider collider;
+    public int velocidad;
+
+    private void OnTrigger (Collider other)
+    {
+        if (other.gameObject.CompareTag "Suelo")
+    }
+}
